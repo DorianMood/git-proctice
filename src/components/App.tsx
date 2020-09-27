a543d28ac2982e23fdbe194186fca663c184a466
nekithockey75 <nikita01051997@gmail.com>
2020-09-27 21:22:16 +0500

    App now is functional component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,19 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
 import "./../assets/scss/App.scss";
 
-class App extends React.Component<Record<string, unknown>, undefined> {
-  public render() {
-    return (
-      <div className="app">
-        <h1>Hello World 1!</h1>
+interface AppProps {}
 
-        <p>Foo to the barz</p>
-        <img src={reactLogo.default} height="480" />
-      </div>
-    );
-  }
-}
+const App = (props: AppProps) => {
+  return (
+    <div className="app">
+      <h1>Hello World1!</h1>
+
+      <p>Foo to the barz</p>
+    </div>
+  );
+};
 
 declare let module: Record<string, unknown>;
 
